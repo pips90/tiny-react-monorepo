f20f032c6d29b6d8937c26696f82b0f0206a228d
Callie <96257895+pips90@users.noreply.github.com>
2025-12-13 19:34:38 -0600

    deploy: web

--- assets/index-DmK8pocA.js
+++ assets/index-DmK8pocA.js
@@ -149,4 +149,4 @@ h2 {
   display: flex;
   justify-content: center;
   margin-top: 8px;
-`;Iv.createRoot(document.getElementById("root")).render(ht.jsx(U.StrictMode,{children:ht.jsx(pp,{children:ht.jsx(wp,{})})}));
+`;Iv.createRoot(document.getElementById("root")).render(ht.jsx(U.StrictMode,{children:ht.jsx(pp,{basename:"/tiny-react-monorepo",children:ht.jsx(wp,{})})}));
